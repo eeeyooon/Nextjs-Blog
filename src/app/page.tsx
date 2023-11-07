12169cc370314516dac72a626c22ff051724f607
jiyoon <vywns9978@gmail.com>
2023-11-07 15:06:43 +0900

    [💄 style] 레이아웃 기본 스타일링 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 export default function Home() {
   return (
-    <section className="p-24 bg-gray-300">
-      <div className="text-blue-500">Hello Next.js!</div>
-    </section>
+    <section className="bg-gray-300 text-blue-500">Hello Next.js!</section>
   );
 }
