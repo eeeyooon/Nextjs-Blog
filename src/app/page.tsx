f222b0a8183d1a318955469fdfbe3de15519845c
jiyoon <vywns9978@gmail.com>
2023-11-07 14:55:42 +0900

    [💄 style] 레이아웃 생성 및 라우팅 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-300">
+    <section className="p-24 bg-gray-300">
       <div className="text-blue-500">Hello Next.js!</div>
-    </main>
+    </section>
   );
 }
