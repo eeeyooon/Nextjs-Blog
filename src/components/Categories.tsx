7e5111d3a5f1366d0c76ebae6c2c4a06676aa091
jiyoon <vywns9978@gmail.com>
2023-11-09 22:11:33 +0900

    [💄 style] posts 페이지 스타일링 적용

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -6,11 +6,19 @@ type Props = {
 
 export default function Categories({ categories, selected, onClick }: Props) {
   return (
-    <section>
-      <h2>Category</h2>
+    <section className="text-center p-4">
+      <h2 className="text-lg font-bold border-b border-sky-600 mb-2">
+        Category
+      </h2>
       <ul>
         {categories.map((category) => (
-          <li key={category} onClick={() => onClick(category)}>
+          <li
+            className={`cursor-pointer hover:text-sky-600 ${
+              category === selected && "text-sky-700 font-bold"
+            }`}
+            key={category}
+            onClick={() => onClick(category)}
+          >
             {category}
           </li>
         ))}
