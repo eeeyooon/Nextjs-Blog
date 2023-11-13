1aa816c509b49d6c1c81d9c5c8069919e5faa14c
jiyoon <vywns9978@gmail.com>
2023-11-13 17:21:17 +0900

    style: 코드 문법 하이라이트 적용 및 img태그 Image컴포넌트로 변경 코드 추가

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -1,10 +1,46 @@
-import Markdown from "react-markdown";
+"use client";
+import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import Image from "next/image";
 
 export default function MarkdownViewer({ content }: { content: string }) {
   return (
-    <Markdown className="prose lg:prose-xl" remarkPlugins={[remarkGfm]}>
+    <ReactMarkdown
+      className="prose max-w-none"
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code({ node, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || "");
+          return match ? (
+            <SyntaxHighlighter
+              language={match[1]}
+              PreTag="div"
+              {...props}
+              ref={null}
+              style={materialDark}
+            >
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          );
+        },
+        img: (image) => (
+          <Image
+            className="w-full max-h-60 object-cover"
+            src={image.src || ""}
+            alt={image.alt || ""}
+            width={500}
+            height={350}
+          />
+        ),
+      }}
+    >
       {content}
-    </Markdown>
+    </ReactMarkdown>
   );
 }
