4bfc21ae719a908ffe84b6d1e9d05b19ef2afb2f
jiyoon <vywns9978@gmail.com>
2023-11-14 21:45:26 +0900

    feat: post 컴포넌트에 이전, 다음 post 데이터 렌더링

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 export default async function PostPage({ params: { slug } }: Props) {
   const post = await getPostData(slug);
-  const { path, title } = post;
+  const { path, title, next, prev } = post;
 
   return (
     <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
@@ -22,6 +22,10 @@ export default async function PostPage({ params: { slug } }: Props) {
         height={420}
       />
       <PostContent post={post} />
+      <section>
+        {prev && <p>{prev.title}</p>}
+        {next && <p>{next.title}</p>}
+      </section>
     </article>
   );
 }
