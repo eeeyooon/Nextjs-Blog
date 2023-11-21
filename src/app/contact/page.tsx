4ca7009d308f7275dc1db4a75a08a9f9ad71a088
jiyoon <vywns9978@gmail.com>
2023-11-21 20:45:20 +0900

    style: 이메일 입력 폼 생성

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,32 @@
+import ContactForm from "@/components/ContactForm";
+import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
+const LINKS = [
+  { icon: <AiFillGithub />, url: "https://github.com/eeeyooon" },
+  { icon: <AiFillLinkedin />, url: "" },
+  { icon: <AiFillYoutube />, url: "" },
+];
 export default function ContactPage() {
-  return <p>연락하기</p>;
+  return (
+    <section className="flex flex-col items-center">
+      <h2 className="text-3xl font-bold my-2">Contact Me</h2>
+      <p>info@dev.com</p>
+      <div>
+        <ul className="flex gap-4 my-2 justify-center">
+          {LINKS.map((link, index) => (
+            <a
+              key={index}
+              href={link.url}
+              target="_blank"
+              rel="noreferrer"
+              className="text-5xl hover:text-blue-500"
+            >
+              {link.icon}
+            </a>
+          ))}
+        </ul>
+        <h2 className="text-3xl font-bold my-8">Or Send me en email</h2>
+        <ContactForm />
+      </div>
+    </section>
+  );
 }
